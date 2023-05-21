1323e87c4f54f79df1e1d499c19466a511fa608a
akhilcj90 <akhilcj@ymail.com>
2023-05-21 10:56:04 +0530

    change_commit

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="mlproject",
+    name="students_score",
     version="0.0.1",
-    author="Krish",
-    author_email="krishnaik06@gmail.com",
+    author="akhi",
+    author_email="akhilcjai@gmail.com",
     packages=find_packages(),
     install_requires=[],
 )
